b19ce301cfaa355984de492369c798b1bcb9f80a
Artur17122006 <artur.cavalcante@alunos.sc.senac.br>
2025-04-22 21:51:41 -0300

    pages
    adicionando pages de login a cadastro de usuário

---
+++ portalCursos/src/app/page/cadastro-user/cadastro-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-user.component.html',
+  styleUrl: './cadastro-user.component.css'
+})
+export class CadastroUserComponent {
+
+}
