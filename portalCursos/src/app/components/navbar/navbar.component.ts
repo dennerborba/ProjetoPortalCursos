c8aa5a6f5bf813fa640a90bb3595273a9eea9192
dennerborba <dennerdeborba@gmail.com>
2025-04-25 15:52:34 -0300

    add navbar

---
+++ portalCursos/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  
+}
