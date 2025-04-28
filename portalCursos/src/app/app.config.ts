c7116c9b4642a4c82b686fbda1bdd499af87a54f
dennerborba <dennerdeborba@gmail.com>
2025-04-28 20:19:31 -0300

    Update app.config.ts

--- portalCursos/src/app/app.config.ts
+++ portalCursos/src/app/app.config.ts
@@ -1,8 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
